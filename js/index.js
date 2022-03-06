5b6ccc5d71864ed85180e9da4bd10f9487e48e23
rubandivyaraj <rubandivyaraj@gmail.com>
2022-03-06 18:22:08 +0530

    Title and Logo added. And Header slider text altered

--- js/index.js
+++ js/index.js
@@ -1,14 +1,12 @@
-var headerName = "Satron Engineering";
-document.title = headerName;
+const sideNav = document.querySelector(".sidenav");
+M.Sidenav.init(sideNav, {});
 
-/** Change Title dynamically when click the nav bar */
-function navTitle(title) {
-  document.title = title.concat(headerName);
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-  const parallaxEffect = document.querySelectorAll(".parallax");
-  M.Parallax.init(parallaxEffect, {});
+const slider = document.querySelector(".slider");
+M.Slider.init(slider, {
+  indicators: false,
+  height: 700,
+  transition: 500,
+  interval: 6000,
 });
 
 function mapLocation() {
@@ -16,3 +14,27 @@ function mapLocation() {
     "https://www.google.co.in/maps/place/Palayur,+P+N+Palayam,+Tamil+Nadu+641044/@11.0170942,76.9765038,15z/data=!3m1!4b1!4m5!3m4!1s0x3ba8584bd4364f8d:0x37275d90116754c8!8m2!3d11.0170946!4d76.9852586";
   window.open(URL, "_blank");
 }
+
+var observer = new IntersectionObserver(
+  function (entries) {
+    if (entries[0].isIntersecting === true) {
+      const counters = document.querySelectorAll(".counter");
+      counters.forEach((counter) => {
+        const updateCount = () => {
+          const target = +counter.getAttribute("data-target");
+          const count = +counter.innerText;
+          if (count < target) {
+            counter.innerText = count + 1;
+            setTimeout(updateCount, 1);
+          } else {
+            counter.innerText = target;
+          }
+        };
+        updateCount();
+      });
+    }
+  },
+  { threshold: [0] }
+);
+
+observer.observe(document.querySelector("#counters"));
